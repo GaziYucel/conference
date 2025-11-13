4b8cbfc1c5e75b846475c0b3a58dda9d38f40aa7
GaziYucel <84437883+GaziYucel@users.noreply.github.com>
2025-11-13 19:19:26 +0100

    OJS 3.5 Compatibility

---
+++ classes/Constants.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file classes/Constants.php
+ *
+ * Copyright (c) 2017-2025 Simon Fraser University
+ * Copyright (c) 2017-2025 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class Constants
+ *
+ * @ingroup plugins_generic_conference
+ *
+ * @brief Constants
+ */
+
+namespace APP\plugins\generic\conference\classes;
+
+class Constants
+{
+}
